ccd234c3f6587faa342e13cd925980ff23165314
tomnDirhub <edelver27@mail.ru>
2024-05-31 20:02:24 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,6 @@
+cnt_of_hws = 12
+spent_time = 1.5
+name_of_course = "Python"
+time_for_1 = cnt_of_hws / spent_time
+print(f"{name_of_course}, всего задач: {cnt_of_hws},затрачено часов: {spent_time}, \
+среднее время выполнения:: {time_for_1} часа.")
